e6930526e996ca1b49fe1f4b09afa443278900c0
Sanjay S K <aksha9538@gmail.com>
2024-03-26 09:15:36 +0530

    ver 0.33

--- Ticket vending machine.c
+++ Ticket vending machine.c
@@ -4,33 +4,36 @@ int main()
     int MenuOption ; // initializing Variable for selecting option;
     do{
 
-    printf(" 1)Ramanagara\n 2)Maddur\n 3)Mandya\n 4)Mysuru\n 5)exit\n enter your option: ");//choose option 1 to 6;
+     //choose option 1-5 for selecting destination address.
+
+    printf(" Here are the list of destination places\n 1)Ramanagara\n 2)Maddur\n 3)Mandya\n 4)Mysuru\n 5)exit\n enter your option: ");
     scanf("%d",&MenuOption);
 
     //here i use switch case for selecting location for generating tickets;
     switch(MenuOption){
 
-    // if case 1 selected ticket booked to Ramanagara follows same ;
+    // if case 1 selected ticket booked to Ramanagara etc follows same ;
     case 1:
         printf("Ramanagara\n");
-        printf("Ticket Booked Successfully\n");
+        printf("Ticket Booked Successfully\nHappy Journey! ");
         break;
     case 2:
         printf("Maddur");
-         printf("Ticket Booked Successfully\n");
+         printf("Ticket Booked Successfully\nHappy Journey!");
         break;
     case 3:
         printf("Mandya");
-         printf("Ticket Booked Successfully\n");
+         printf("Ticket Booked Successfully\nHappy Journey");
         break;
     case 4:
         printf("Mysuru");
-         printf("Ticket Booked Successfully\n");
+         printf("Ticket Booked Successfully\nHappy Journey");
         break;
      case 5:
-        printf("Exiting program\n");
+        printf("Happy journey\n");
         return 0; // Exit the program
-     //In case if you choose option 6 it shows invalid option;
+
+    //In case if you choose option 6 it shows invalid option;
     case 6:
         printf("invalid option\n");
         printf("Enter Valid option\n");
@@ -42,7 +45,7 @@ int main()
                 printf("Enter a valid option\n");
                 printf("Ticket Booking Failed\n");
         }
-    } while (MenuOption < 1 || MenuOption > 4); // Loop until a valid option is entered
+    } while (MenuOption < 1 || MenuOption > 5); // Loop until a valid option is entered
 
     return 0;
 }
