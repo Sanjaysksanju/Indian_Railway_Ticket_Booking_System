82e09b4ce0ead936fc2462b127f3998e2346b053
Sanjay S K <aksha9538@gmail.com>
2024-03-27 13:03:28 +0530

    ver 0.35

--- Ticket vending machine.c
+++ Ticket vending machine.c
@@ -1,46 +1,57 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main() {
     int MenuOption; // initializing Variable for selecting option;
+    int numPersons; // Variable to store the number of persons
 
     do {
         // Choose option 1-5 for selecting destination address.
-        printf("\nHere are the list of destination places\n1) Ramanagara\n2) Maddur\n3) Mandya\n4) Mysuru\n5) Exit\nEnter your option: ");
+        printf("Here are the list of destination places\n1) Ramanagara\n2) Maddur\n3) Mandya\n4) Mysuru\n5) Exit\nEnter your option: ");
         scanf("%d", &MenuOption);
+
+        // Validate the menu option
+        if (MenuOption < 1 || MenuOption > 5) {
+            printf("Invalid option\n");
+            continue;
+        }
+
+        if (MenuOption == 5) {
+            printf("Happy journey\n");
+            break;
+        }
+
+        // Ask the user to input the number of persons
+        printf("How many persons are booking tickets? ");
+        scanf("%d", &numPersons);
+
+        // Validate the number of persons
+        if (numPersons <= 0) {
+            printf("Invalid number of persons. Please enter a valid number.\n");
+            continue;
+        }
 
         // here I use switch case for selecting location for generating tickets
         switch (MenuOption) {
             // if case 1 selected ticket booked to Ramanagara etc follows same
             case 1:
-                printf("Ramanagara\n");
-                printf("Ticket Booked Successfully\nHappy Journey! ");
+                printf("%d ticket(s) booked to Ramanagara\n", numPersons);
+                printf("Ticket(s) Booked Successfully\nHappy Journey! ");
                 break;
             case 2:
-                printf("Maddur ");
-                printf("Ticket Booked Successfully\nHappy Journey!");
+                printf("%d ticket(s) booked to Maddur\n", numPersons);
+                printf("Ticket(s) Booked Successfully\nHappy Journey!");
                 break;
             case 3:
-                printf("Mandya ");
-                printf("Ticket Booked Successfully\nHappy Journey!");
+                printf("%d ticket(s) booked to Mandya\n", numPersons);
+                printf("Ticket(s) Booked Successfully\nHappy Journey!");
                 break;
             case 4:
-                printf("Mysuru ");
-                printf("Ticket Booked Successfully\nHappy Journey!");
+                printf("%d ticket(s) booked to Mysuru\n", numPersons);
+                printf("Ticket(s) Booked Successfully\nHappy Journey!");
                 break;
-            case 5:
-                printf("Happy journey\n");
-                return 0; // Exit the program
-            // In case if you choose option 6 it shows invalid option
-            case 6:
-                printf("Invalid option\n");
-                printf("Enter Valid option\n");
-                printf("Ticket Booking Failed\n");
-                break;
-            default:
-                printf("Invalid option\n");
-                printf("Enter a valid option\n");
-                printf("Ticket Booking Failed\n");
         }
-    } while (MenuOption != 5); // Loop until a valid option is entered
+
+    } while (MenuOption != 5); // Loop until the user chooses to exit
 
     return 0;
 }
